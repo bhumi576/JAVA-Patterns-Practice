5990c6a4d062ad25c80db5a94058571a8970015e
bhumi576 <bbhumi576@gmail.com>
2026-02-05 19:16:44 +0530

    Add files via upload
    📄 Java Basics Practice (Beginner Notes)
    
    This file contains basic Java concepts practiced step by step with comments for better understanding.
    It is written in a learning / revision style, where concepts are explained alongside code.
    
    Topics covered in this file include:-
    
    Printing output (print vs println)
    New line handling using \n
    Variable declaration and initialization
    Rules for variable redeclaration
    Taking input using Scanner
    Understanding input buffer issues (nextInt, nextFloat, nextLine)
    Conditional statements (if-else)
    Switch-case statement basics
    
    The code is intentionally kept simple, commented, and beginner-friendly, making it easy to revise Java fundamentals and understand common mistakes faced by beginners.

---
+++ 1basics.java
@@ -0,0 +1,118 @@
+// import java.util.Scanner;
+// public class yt {
+
+// 	public static void main(String[] args) {
+//	System.out.print("HEllo WOrld!!");
+		
+//		---> PRINTING IN NExT LINE HAS TWO METHODS 
+//		1.
+//	System.out.println("hello bhumi");
+//	System.out.println("hello mannu!!");
+//		bhumi
+//		mannu
+//	}
+
+//		2.
+//		System.out.print("hello\nworld");
+//		hello
+//		world
+		
+		
+//		VARIABLES
+		// int x=5;
+		// System.out.println(x);
+
+		// int y;
+		// y=20;
+		// System.out.println(y);
+// 		System.out.println(x + " " + y);
+
+
+      //---> DECLARE WILL BE DONE ONCE
+	//   int x=9;
+	//   int x=8; // error 
+	// x=10; // no problem
+
+
+
+
+	// --------------------------------------------------------
+	//               INPUT
+		// Scanner sc=new Scanner(System.in);
+		// int x=sc.nextInt();
+		// float f=sc.nextFloat();
+		// String s=sc.nextLine();
+
+		// System.out.println(x);
+		// System.out.println(f);
+		// System.out.println(s);
+	// * NOTE *--->> i entered 3
+	//                        3.4
+	//  when i press enter after 3.4 it end the code-->> and output is 3 and 3.4  "space in next line" only as compiler read
+// 	3  -- input
+// 3.4  --input
+
+// 3    -- output
+// 3.4  --output
+//   --output [empty space]
+	// it takes "enter" as an input in string area
+
+// -->> so enter in one line 
+// input--- 3 4.5 bhumi
+// 3
+// 4.5
+// bhumi
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ------------------------------------------------------------------------------------------------------------------------------
+//                 CONDITIONAL STAEMENTS
+
+//                        IF ELSE
+// Scanner sc=new Scanner(System.in);
+// System.out.println("enter age: ");
+// int age=sc.nextInt();
+// if(age>18){
+// 	System.out.println("you are eleigibble");
+// }
+// else{
+// 	System.out.println("not elegible");
+// }
+
+
+
+
+// .................... SWITCH STATEMENT ...................
+
+
+// Scanner sc=new Scanner(System.in);
+// int button=sc.nextInt();
+
+// switch(button){
+// 	case 1:
+// 		System.out.println("case 1");
+// 		break;
+// 	case 2:
+// 		System.out.println("case 2");
+// 		break;
+// 	case 3:
+// 		System.out.println("case 3");
+// 		break;
+// 	default:
+// 		System.out.println("invalid");
+// }
+
+
+
+// }
+// }
